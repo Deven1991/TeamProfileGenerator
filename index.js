9465ebadd70da03f64e9ae7585cb248cb7cb8d6c
Melissa Deven <99757260+Deven1991@users.noreply.github.com>
2022-06-12 16:08:04 -0500

    updated index.js file path

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const { generateTemplate } = require("./src/page-template")
+const { generateTemplate } = require("./src/template")
 
 var answers = [];
 const mainQuestions = [{
